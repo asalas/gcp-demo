f8333a09d57a838ef976036816c2320813311de1
Antonino Salas <antonino.salas@objectwave.com>
2017-10-26 12:52:05 -0500

    Se agrega el uso del servicio faltante.

--- src/main/java/io/swagger/api/HolaApiController.java
+++ src/main/java/io/swagger/api/HolaApiController.java
@@ -1,5 +1,6 @@
 package io.swagger.api;
 
+import com.cirrocode.gcp.demo.service.SaludoService;
 import io.swagger.model.HolaResponse;
 
 import io.swagger.annotations.*;
@@ -8,26 +9,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
-import javax.validation.constraints.*;
-import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-10-26T05:27:28.167Z")
 
 @Controller
 public class HolaApiController implements HolaApi {
 
-
+    @Autowired SaludoService saludoService;
 
     public ResponseEntity<HolaResponse> saludo(@ApiParam(value = "Nombre de la persona a saludar",required=true ) @PathVariable("nombre") String nombre) {
-        // do some magic!
-        return new ResponseEntity<HolaResponse>(HttpStatus.OK);
+        final HolaResponse response = this.saludoService.saludar(nombre);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
