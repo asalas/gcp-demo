403a8cc9697e7993d459d86f215fa4775a65384f
Antonino Salas <antonino.salas@objectwave.com>
2017-10-28 14:04:01 -0500

    se remueve operacion en repository

--- src/main/java/com/cirrocode/gcp/demo/repository/SaludoRepository.java
+++ src/main/java/com/cirrocode/gcp/demo/repository/SaludoRepository.java
@@ -1,9 +1,7 @@
 package com.cirrocode.gcp.demo.repository;
 
 import com.cirrocode.gcp.demo.entity.Saludo;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -13,6 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SaludoRepository extends CrudRepository<Saludo, Integer>
 {
-    @Query("SELECT * FROM Saludo AS s WHERE s.mensaje = :mensj")
-    Saludo findByMensaje(@Param("mensj") String mensj);
+
 }
